a6da35fd933a333298ef8ec9b3dcc1f946da1b93
Stanley Jiles <sjiles1@my.westga.edu>
2020-02-23 20:26:08 -0500

    Merge

--- code/SteamScout/src/com/steamscout/application/Main.java
+++ code/SteamScout/src/com/steamscout/application/Main.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import com.steamscout.application.view.UIFilePaths;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
