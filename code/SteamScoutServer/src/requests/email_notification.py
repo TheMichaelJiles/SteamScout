6553fb7a3626c119b2a38a0f7c81d1ed53023731
Nathaniel Lightholder <nlighth1@my.westga.edu>
2020-03-12 13:39:17 -0400

    Added class for email notifications

---
+++ code/SteamScoutServer/src/requests/email_notification.py
@@ -0,0 +1,18 @@
+'''
+Created on Mar 12, 2020
+
+@author: Nathan Lightholder
+'''
+
+class email_notification(object):
+    '''
+    class is responsible for creating the emails 
+    to notify uses of price changes
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
