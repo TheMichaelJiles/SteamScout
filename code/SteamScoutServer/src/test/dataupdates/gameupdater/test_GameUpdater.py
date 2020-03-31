ae1b94f519941481eb33e63a53ac2944d2041a98
Nathaniel Lightholder <nlighth1@my.westga.edu>
2020-03-30 21:57:19 -0400

    Uncommented method

--- code/SteamScoutServer/src/test/dataupdates/gameupdater/test_GameUpdater.py
+++ code/SteamScoutServer/src/test/dataupdates/gameupdater/test_GameUpdater.py
@@ -3,12 +3,8 @@ Created on Mar 29, 2020
 
 @author: Nathan Lightholder
 '''
-<<<<<<< HEAD
-import unittest
-import json
-=======
+
 import unittest, os, json
->>>>>>> branch 'master' of https://github.com/UWG-Software-Engineering/UWG-SE2-Spring20-Group4.git
 from mock import patch
 from dataupdates.gameupdater import GameUpdater
 from api.gamepull import GamePull
@@ -16,14 +12,7 @@ from api.gamepull import GamePull
 
 class TestGameUpdater(unittest.TestCase):
 
-<<<<<<< HEAD
-    @patch.object(json, 'dump')
-    def test_game_updater_test(self, mock):
-=======
-
     def test_game_updater_test(self):
-        
->>>>>>> branch 'master' of https://github.com/UWG-Software-Engineering/UWG-SE2-Spring20-Group4.git
         updater = GameUpdater()
         result = updater.fill_game_table(True)
         
@@ -42,18 +31,12 @@ class TestGameUpdater(unittest.TestCase):
         with open(path, 'w') as jsonfile:
             json.dump(test_table, jsonfile)
     
-    '''@patch.object(GamePull, 'pull_games')    
+    @patch.object(GamePull, 'pull_games')    
     def test_game_updater_fails(self, mock):
         updater = GameUpdater()
         mock.return_value = '0'
         result = updater.fill_game_table(True)
-        
-<<<<<<< HEAD
-        self.assertFalse(result)'''
-=======
         self.assertFalse(result)
-
->>>>>>> branch 'master' of https://github.com/UWG-Software-Engineering/UWG-SE2-Spring20-Group4.git
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
