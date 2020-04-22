35f93de4eaaefaaaa63b0027124248658063a8bd
TheMichaelJiles <sjiles1@my.westga.edu>
2020-04-22 12:20:42 -0400

    Added custom NotificationListCell
    -Created fxml for NotificationListCell
    -Enter will not log you in after entering valid credentials on Login
    Page
    -Created NotificationListCellCodeBehind

--- code/SteamScout/src/com/steamscout/application/view/UIFilePaths.java
+++ code/SteamScout/src/com/steamscout/application/view/UIFilePaths.java
@@ -19,5 +19,6 @@ public class UIFilePaths {
 	public static final String LINK_STEAM_WISHLIST_PAGE = "LinkSteamWishlistPage.fxml";
 	public static final String SHARE_WATCHLIST_PAGE = "ShareWatchlistPage.fxml";
 	public static final String SHARE_WATCHLIST_USERNAME_PAGE = "ShareWatchlistUsernamePage.fxml";
+	public static final String NOTIFICATION_LIST_CELL_FILENAME = "NotificationListCell.fxml";
 	
 }
